2c8bbeb2a0a0218ec4cc6abfcf6471e936775ed0
KonstantinMukhin <kostik.mukhin.1995@gmail.com>
2022-09-09 22:08:43 +0300

    crete table and delete elements table

---
+++ src/component/users.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import API from "../api";
+
+const Users= () => {
+    const [users, setUsers] = useState(API.users.fetchAll());
+
+    const handleDelete = (userId) => {
+        setUsers(prevState => prevState.filter(user => user !== userId))
+    };
+
+    const handlePhrase = (number) => {
+        if (number === 1) {
+            return 'человек тусанет';
+        } else if (number > 1 && number <= 4) {
+            return 'человека тусанут';
+        } else if (number > 4){
+            return 'человек тусанут';
+        };
+    };
+
+    return (
+        <>
+            <h2>
+                <span className={"badge " + (users.length > 0 ? 'bg-primary' : 'bg-danger')}>
+                    {users.length > 0 ?
+                    `${users.length + ' ' + handlePhrase(users.length)} с тобой сегодня` : 'Никто с тобой не тусанет'} 
+                </span>
+            </h2>
+
+            {users.length > 0 && (<table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Имя</th>
+                        <th scope="col">Качества</th>
+                        <th scope="col">Профессия</th>
+                        <th scope="col">Встретился, раз</th>
+                        <th scope="col">Оценка</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map(user => (
+                        <tr key={user._id}>
+                            <td scope="row">{user.name}</td>
+                            <td>{user.qualities.map(item => 
+                                <span className={'badge bg-'+item.color+' m-1'} key={item._id}>{item.name}</span>)}
+                            </td>
+                            <td>{user.profession.name}</td>
+                            <td>{user.completedMeetings}</td>
+                            <td>{user.rate+'/5'}</td> 
+                            <td><button class="btn btn-danger" onClick={() => handleDelete(user)}>delete</button></td>
+                        </tr>
+                    ))}    
+                </tbody>
+            </table>)}
+        </>
+    );
+};
+
+
+export default Users;
