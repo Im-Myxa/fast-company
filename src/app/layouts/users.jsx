8cccdde161538b1c1f1f414b6f9442704f7ed26c
KonstantinMukhin <kostik.mukhin.1995@gmail.com>
2022-11-29 05:48:23 +0300

    add http service

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -5,21 +5,24 @@ import { useParams } from "react-router-dom";
 import EditUserPage from "../component/page/editUserPage/editUserPage";
 import UserPage from "../component/page/userPage";
 import UsersListPage from "../component/page/usersListPage";
+import UserProvider from "../hooks/useUsers";
 
 const Users = () => {
     const { userId, edit } = useParams();
 
     return (
         <>
-            {edit ? (
-                <EditUserPage userId={userId} />
-            ) : userId ? (
-                <UserPage userId={userId} />
-            ) : (
-                <UsersListPage />
-            )}
-            {/* {userId ? <UserPage userId={userId} /> : <UsersListPage />}
+            <UserProvider>
+                {edit ? (
+                    <EditUserPage userId={userId} />
+                ) : userId ? (
+                    <UserPage userId={userId} />
+                ) : (
+                    <UsersListPage />
+                )}
+                {/* {userId ? <UserPage userId={userId} /> : <UsersListPage />}
             {edit && <EditUserPage userId={userId} />} */}
+            </UserProvider>
         </>
     );
 };
